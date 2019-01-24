a9425a1e0167cec55984e8159df7d232b6446850
Kobus Myburgh <kobus@thumbzup.com>
2019-01-24 11:00:45 +0200

    JIRA 3 and JIRA 4 conflicts resolved

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 Hello
 Kobus was hier
 Kobus was weer hier! Leon was hier. PANS WAS HIER !
-<<<<<<< HEAD
 Feature 1 Changed by Kobus while  in centurion
 Feature 2
 Kobus was weer hier!
 Kobus was weer 'n keer hier!
 Kobus was hier terwyl in Centurion
-=======
-Feature 1
-Feature 2 changed by Kobus while in Centurion
->>>>>>> JIRA4
